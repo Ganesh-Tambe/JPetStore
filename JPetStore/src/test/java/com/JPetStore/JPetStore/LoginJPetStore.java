2f10c638a980dd4d884e16266ebdf02bd0f951ec
Ganesh-Tambe <ganesh.tambe@expleogroup.com>
2024-01-24 11:55:13 +0530

    Added Wait

--- JPetStore/src/test/java/com/JPetStore/JPetStore/LoginJPetStore.java
+++ JPetStore/src/test/java/com/JPetStore/JPetStore/LoginJPetStore.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -14,12 +16,14 @@ import junit.framework.Assert;
 
 public class LoginJPetStore {
 	WebDriver driver;
+	WebDriverWait wait;
  @BeforeTest
  public void beforeTest()
  {
 	 driver=new ChromeDriver();
 	  driver.get("https://petstore.octoperf.com/actions/Catalog.action");
 	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	  wait =new WebDriverWait(driver, Duration.ofSeconds(10));
  }
  //Login Functionality
 	@Test
@@ -37,7 +41,7 @@ public class LoginJPetStore {
 	@Test
 	public void  logout()
 	{
-		driver.findElement(By.xpath("//a[text()='Sign Out']")).click();
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//a[text()='Sign Out']")))).click();
 		Assert.assertEquals(driver.getCurrentUrl(), "https://petstore.octoperf.com/actions/Catalog.action");
 	}
 	
